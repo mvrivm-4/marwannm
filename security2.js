1d9476b2eb7658b9c8eb838697e497f543e652fa
mvrivm-4 <mrwanmohamd206@gmail.com>
2024-12-24 00:19:16 +0200

    Add files via upload

---
+++ security2.js
@@ -0,0 +1,80 @@
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let carousel = document.querySelector('.carousel');
+let items = document.querySelectorAll('.carousel .item');
+let countItem = items.length;
+let active = 1;
+
+const updateItems = () => {
+    items.forEach((item, index) => {
+        item.classList.remove('active', 'other_1', 'other_2');
+        if (index === active) {
+            item.classList.add('active');
+        } else if (index === (active + 1) % countItem) {
+            item.classList.add('other_1');
+        } else if (index === (active + 2) % countItem) {
+            item.classList.add('other_2');
+        }
+    });
+};
+
+function openNav() {
+    document.getElementById("mySideNav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySideNav").style.width = "0";
+}
+
+next.onclick = () => {
+    carousel.classList.remove('prev');
+    carousel.classList.add('next');
+    active = (active + 1) % countItem;
+    updateItems();
+};
+
+prev.onclick = () => {
+    carousel.classList.remove('next');
+    carousel.classList.add('prev');
+    active = (active - 1 + countItem) % countItem;
+    updateItems();
+};
+
+updateItems();
+let darks = document.querySelector(".darks");
+let lights = document.querySelector(".lights");
+let moka = document.querySelector("#moka")
+
+
+darks.addEventListener("click", function() {
+    document.body.style.backgroundColor = 'black'
+    document.body.style.color = "white"
+    darks.classList.add("hide")
+    lights.classList.remove("hide")
+    moka.style.color = "black"
+
+
+})
+lights.addEventListener("click", function() {
+    document.body.style.backgroundColor = 'white'
+    document.body.style.color = "black"
+    lights.classList.add("hide")
+    darks.classList.remove("hide")
+    moka.style.color = "black"
+
+
+
+})
+let btns = document.getElementById("btns")
+window.addEventListener("scroll", function() {
+    if (this.scrollY >= 400) {
+        btns.style.display = "block"
+
+    } else if (this.scrollY <= 400) {
+        btns.style.display = "none"
+    } else {
+        btns.style.backgroundColor = "red"
+        btns.style.color = "white"
+
+    }
+})
